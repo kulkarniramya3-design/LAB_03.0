d02e704495c8c0601bb83dd512ac13015140089e
Ramya patil kulkarni <kulkarniramya3@gmail.com>
2026-09-16 16:27:47 +0530

    save

---
+++ lab_3.py
@@ -0,0 +1,15 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+
+print("Before swapping:")
+print("First number =", a)
+print("Second number =", b)
+
+
+a, b = b, a
+
+
+print("After swapping:")
+print("First number =", a)
+print("Second number =", b)
